29ca0c75ed62b49bd5fcc0e51216c1815f4ab16a
hp113 <hariprasad2315@gmail.com>
2024-06-04 20:33:11 +0530

    changed design of participation page

--- app/routes/dashboard.participation.tsx
+++ app/routes/dashboard.participation.tsx
@@ -66,7 +66,7 @@ export default function DashboardParticipation() {
       </h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {participants.map((participant) => (
-          <div key={participant.id} className="bg-white shadow rounded-lg p-4">
+          <div key={participant.id} className="bg-gray-300 shadow rounded-lg p-4">
             <Card >
               <CardHeader>
                 <h2 className="font-bold">{participant.event_name}</h2>
